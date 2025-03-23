9a94e818b1a0463d6b0cb44dea9f96fdafad7660
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-23 08:47:23 -0500

    routing pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Welcome from "./routes";
+import Welcome from "./Welcome";
 
 export function App() {
   const [showHome, setShowHome] = useState(false);
@@ -11,18 +11,6 @@ export function App() {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -40,10 +28,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
