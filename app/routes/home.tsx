f62189b8a784d00704974630b87379e685a13c18
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-23 10:27:39 -0500

    fix?

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-enter justify-center h-screen bg-green-900 text-white">
+    <div className="flex items-enter justify-center h-screen bg-primary text-white">
       <h1 className="text-5xl font-bold">Welcome to My Portfolio</h1>
       </div>
   );
