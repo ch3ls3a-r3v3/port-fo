c0fafc228bb4a88f28738810577addbe888a3a04
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-23 08:09:38 -0500

    Home.tsx fixed type error

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,6 @@ export default function Home() {
   return (
     <div className="flex items-enter justify-center h-screen bg-green-900 text-white">
       <h1 className="text-5xl font-bold"> Welcome to My Portfolio!</h1>
-      </div>)
+      </div>
   );
 }
